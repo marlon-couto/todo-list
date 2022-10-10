9f8b0ee731cb0df9185ecb1fd63b5b26fce0b503
Marlon Couto <marloncouto199@gmail.com>
2022-10-10 13:53:06 -0300

    Adiciona opção de marcar itens da lista como completos
    Signed-off-by: Marlon Couto <marloncouto199@gmail.com>

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const criarTarefa = document.querySelector('#criar-tarefa');
 const listaDeTarefas = document.querySelector('#lista-tarefas');
+const redefinir = document.querySelector('#apaga-tudo');
 
 criarTarefa.addEventListener('click', () => {
   const textoDaTarefa = document.querySelector('#texto-tarefa');
@@ -20,14 +21,12 @@ listaDeTarefas.addEventListener('click', (event) => {
 });
 
 listaDeTarefas.addEventListener('dblclick', (event) => {
-  if (event.target.className !== 'completed') {
-    event.target.classList.add('completed');
-  } else {
+  if (event.target.className === 'completed') {
     event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
   }
 });
-
-const redefinir = document.querySelector('#apaga-tudo');
 
 redefinir.addEventListener('click', () => {
   listaDeTarefas.innerHTML = '';
