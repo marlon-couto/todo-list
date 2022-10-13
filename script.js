e029c081ca064c0d90561a67750aeb1dd55c8860
Marlon Couto <marloncouto199@gmail.com>
2022-10-13 10:41:36 -0300

    Finaliza projeto Lista de Tarefas
    Signed-off-by: Marlon Couto <marloncouto199@gmail.com>

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const criarTarefa = document.querySelector('#criar-tarefa');
 const listaDeTarefas = document.querySelector('#lista-tarefas');
 const redefinir = document.querySelector('#apaga-tudo');
 const removerFinalizados = document.querySelector('#remover-finalizados');
+const removerSelecionado = document.querySelector('#remover-selecionado');
+const paraCima = document.querySelector('#mover-cima');
+const paraBaixo = document.querySelector('#mover-baixo');
 
 const saveList = () => {
   localStorage.setItem('listaDeTarefas', listaDeTarefas.innerHTML);
@@ -18,16 +21,16 @@ criarTarefa.addEventListener('click', () => {
 
 listaDeTarefas.addEventListener('click', (event) => {
   const tarefas = document.querySelectorAll('li');
-  event.target.style.setProperty('background-color', 'gray');
+  event.target.classList.add('selected');
   tarefas.forEach((element) => {
-    if (element.style.backgroundColor === 'gray' && element !== event.target) {
-      element.style.removeProperty('background-color');
+    if (element !== event.target && element.classList.contains('selected')) {
+      element.classList.remove('selected');
     }
   });
 });
 
 listaDeTarefas.addEventListener('dblclick', (event) => {
-  if (event.target.className === 'completed') {
+  if (event.target.classList.contains('completed')) {
     event.target.classList.remove('completed');
   } else {
     event.target.classList.add('completed');
@@ -46,6 +49,36 @@ removerFinalizados.addEventListener('click', () => {
     listaDeTarefas.removeChild(tarefasFinalizadas[index]);
   }
   saveList();
+});
+
+paraCima.addEventListener('click', () => {
+  const selected = document.querySelector('.selected');
+  if (selected !== null && selected !== listaDeTarefas.firstElementChild) {
+    const previous = document.querySelector('.selected').previousSibling;
+    const oldValue = document.querySelector('.selected').outerHTML;
+    selected.outerHTML = previous.outerHTML;
+    previous.outerHTML = oldValue;
+    saveList();
+  }
+});
+
+paraBaixo.addEventListener('click', () => {
+  const selected = document.querySelector('.selected');
+  if (selected !== null && selected !== listaDeTarefas.lastElementChild) {
+    const next = document.querySelector('.selected').nextSibling;
+    const oldValue = document.querySelector('.selected').outerHTML;
+    selected.outerHTML = next.outerHTML;
+    next.outerHTML = oldValue;
+    saveList();
+  }
+});
+
+removerSelecionado.addEventListener('click', () => {
+  const selected = document.querySelector('.selected');
+  if (selected !== null) {
+    selected.remove();
+    saveList();
+  }
 });
 
 window.onload = () => {
